cb4a96d9743c9d8a079fc2a0928d687d91bc65f8
Vincent MacKay <vince.mackay@gmail.com>
2022-08-10 17:51:34 -0400

    Updated and added some functions

--- CSTUtils.py
+++ CSTUtils.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug  2 17:02:11 2022
+Created on Tue Aug  2 17:02:11 2022.
 
 @author: vincent
 """
 
 import matplotlib.pyplot as plt
 import numpy as np
+import copy
 
 class CSTBeam():
     """
+    Class containing beams as simulated by CST.
+    
     This class take a folder where a beam has been converted to the
-    "all_*.npy" formats, loads it, an 
+    "all_*.npy" formats, and loads it.
     """
+    
     def __init__(self,beams_folder, zenith_rot = 0,ns_rot = 0, ew_rot = 0, load_comps=False,load_axratios=False):
-        self.beams_dir = np.load(beams_folder+'all_beams_dir.npy')
+        self.directivity = np.load(beams_folder+'all_directivity.npy')
         self.freqs = np.load(beams_folder+'all_freqs.npy')
         self.phi = np.load(beams_folder + 'all_phi.npy')
         self.theta = np.load(beams_folder + 'all_theta.npy')
@@ -23,7 +27,7 @@ class CSTBeam():
         self.theta_step = self.theta[0][1]
         self.freq_min = self.freqs[0]
         self.freq_step = self.freqs[1] - self.freqs[0]
-        self.gains = np.max(self.beams_dir,(2,3))
+        self.gains = np.max(self.directivity,(2,3))
         self.wl = 2.99792458e8 / (self.freqs * 1e9) #wavelength, in meters
         self.A_e = ( self.gains * self.wl ** 2 ) / 4 / np.pi
         if load_comps:
@@ -60,12 +64,52 @@ class CSTBeam():
             # Reindex
             i_new_theta = (new_theta / self.theta_step).astype('int')
             i_new_phi = (new_phi / self.phi_step).astype('int')
-            self.beams_dir = self.beams_dir[:,:,i_new_phi,i_new_theta]
+            self.directivity = self.directivity[:,:,i_new_phi,i_new_theta]
         
-
+    def rotate(self, zenith_rot = 0, ns_rot = 0, ew_rot = 0):
+        """
+        Rotate the beam.
+        
+        This function rotates the beam by the desired angles.
+        """
+        # Make a copy of the beam
+        new_beam = copy.deepcopy(self)
+                
+        # Make sure the rotations are at the right resolution
+        ns_rot = new_beam.theta_step*np.round(ns_rot/new_beam.theta_step)
+        ew_rot = new_beam.theta_step*np.round(ew_rot/new_beam.theta_step)
+        zenith_rot = new_beam.phi_step*np.round(zenith_rot/new_beam.phi_step)
+        
+        # I do the rotations using this method:
+        # https://stla.github.io/stlapblog/posts/RotationSphericalCoordinates.html
+        
+        a_x = ew_rot * (np.pi/180)
+        a_y = ns_rot * (np.pi/180)
+        a_z = zenith_rot * (np.pi/180)
+                
+        R_x = np.array([ [np.cos(a_x/2) , -1j*np.sin(a_x/2)] , [-1j*np.sin(a_x/2) , np.cos(a_x/2)] ])
+        R_y = np.array([ [ np.cos(a_y/2) , -np.sin(a_y/2) ] , [ np.sin(a_y/2) , np.cos(a_y/2) ] ])
+        R_z = np.array([ [ np.exp(-1j * a_z/2) , 0 ] , [ 0 , np.exp(1j * a_z/2) ] ])
+        
+        for R in [R_z,R_x,R_y]: # this determines the order of the rotations
+            t = new_beam.theta * (np.pi/180)
+            p = new_beam.phi * (np.pi/180)
+            psi = np.array([np.cos(t/2), np.exp(1j * p) * np.sin(t/2)])
+            psi[0] = R[0,0] * psi[0] + R[0,1] * psi[1]
+            psi[1] = R[1,0] * psi[0] + R[1,1] * psi[1]
+            new_theta = (180/np.pi) * 2*np.arctan(np.abs(psi[1])/np.abs(psi[0]))
+            new_phi = (180/np.pi) * (np.angle(psi[1]) - np.angle(psi[0]))
+            # Reindex
+            i_new_theta = (new_theta / new_beam.theta_step).astype('int')
+            i_new_phi = (new_phi / new_beam.phi_step).astype('int')
+            new_beam.directivity = new_beam.directivity[:,:,i_new_phi,i_new_theta]
          
+        return new_beam
+        
     def plot_1d_cart(self,phi_cut,i_freq,pol=0, dB = True):
         """
+        Plot a 1D beam cut (cartesian).
+        
         This function plots the beam in 1D in cartesian coordinates,
         along the cut phi_cut, at frequency index i_freq, for polarization pol.
         """
@@ -82,7 +126,7 @@ class CSTBeam():
         ylabel = r'$I(\theta)$ (arbitrary units)'
         if pol==-1:
             #print('Plotting (pol_0 ^ 2 + pol_1 ^ 2) ^ 0.5')
-            power = (self.beams_dir[0,:,:,:] ** 2. + self.beams_dir[1,:,:,:] ** 2.) ** 0.5
+            power = (self.directivity[0,:,:,:] ** 2. + self.directivity[1,:,:,:] ** 2.) ** 0.5
             if dB:
                 power = 10 * np.log10(power)
                 ylabel = r'$I(\theta)$ (dB)'
@@ -92,7 +136,7 @@ class CSTBeam():
             ax.plot(-self.theta[i_phi_cut_2] , power[i_freq][i_phi_cut][:], **line_props)
         else:
             #print('Plotting pol_{:d}'.format(pol))
-            power = self.beams_dir
+            power = self.directivity
             if dB:
                 power = 10 * np.log10(power)
                 ylabel = r'$I(\theta)$ (dB)'
@@ -108,6 +152,8 @@ class CSTBeam():
 
     def plot_1d_polar(self,phi_cut,i_freq,pol=0, dB = True):
         """
+        Plot a 1D beam cut (polar).
+        
         This function plots the beam in 1D in polar coordinates,
         along the cut phi_cut, at frequency index i_freq, for polarization pol.
         """
@@ -125,7 +171,7 @@ class CSTBeam():
         ylabel = r'$I(\theta)$ (arbitrary units)'
         if pol==-1:
             #print('Plotting (pol_0 ^ 2 + pol_1 ^ 2) ^ 0.5')
-            power = (self.beams_dir[0,:,:,:] ** 2. + self.beams_dir[1,:,:,:] ** 2.) ** 0.5
+            power = (self.directivity[0,:,:,:] ** 2. + self.directivity[1,:,:,:] ** 2.) ** 0.5
             if dB:
                 power = 10 * np.log10(power)
                 ylabel = r'$I(\theta)$ (dB)'
@@ -135,7 +181,7 @@ class CSTBeam():
             ax.plot(-(np.pi / 180)*self.theta[i_phi_cut_2] , power[i_freq][i_phi_cut][:], **line_props)
         else:
             #print('Plotting pol_{:d}'.format(pol))
-            power = self.beams_dir
+            power = self.directivity
             if dB:
                 power = 10 * np.log10(power)
                 ylabel = r'$I(\theta)$ (dB)'
@@ -150,6 +196,8 @@ class CSTBeam():
         
     def plot_2d_uv(self, i_freq, i_pol, dB = True, front_cutoff = 90, back_cutoff = -1):
         """
+        Plot a 2D beam (uv).
+        
         This function plots the beam in a 2D uv plot.
         I need to check whether my definition of uv coordinates is good.
         """
@@ -164,15 +212,15 @@ class CSTBeam():
 
         fig,ax = plt.subplots(1,1,figsize=[10,10])
         if dB:
-            vmin = np.min(10*np.log10(self.beams_dir[i_pol,i_freq,:,:]))
-            vmax = np.max(10*np.log10(self.beams_dir[i_pol,i_freq,:,:]))
+            vmin = np.min(10*np.log10(self.directivity[i_pol,i_freq,:,:]))
+            vmax = np.max(10*np.log10(self.directivity[i_pol,i_freq,:,:]))
             cb_label = 'dB'
-            image2d = ax.pcolormesh(u[:,to_plot],v[:,to_plot],10*np.log10(self.beams_dir[i_pol,i_freq][:,to_plot]),shading='auto',vmin = vmin, vmax = vmax)
+            image2d = ax.pcolormesh(u[:,to_plot],v[:,to_plot],10*np.log10(self.directivity[i_pol,i_freq][:,to_plot]),shading='auto',vmin = vmin, vmax = vmax)
         else:
-            vmin = np.min(self.beams_dir[i_pol,i_freq,:,:])
-            vmax = np.max(self.beams_dir[i_pol,i_freq,:,:])
+            vmin = np.min(self.directivity[i_pol,i_freq,:,:])
+            vmax = np.max(self.directivity[i_pol,i_freq,:,:])
             cb_label = 'Intensity (arbitrary units)'
-            image2d = ax.pcolormesh(u[:,to_plot],v[:,to_plot],self.beams_dir[i_pol,i_freq][:,to_plot],shading='auto',vmin = vmin, vmax = vmax)
+            image2d = ax.pcolormesh(u[:,to_plot],v[:,to_plot],self.directivity[i_pol,i_freq][:,to_plot],shading='auto',vmin = vmin, vmax = vmax)
         
         ax.set_aspect('equal')
         ax.set_xlim([-1,1])
@@ -183,20 +231,22 @@ class CSTBeam():
     
     def plot_2d_cart(self, i_freq, i_pol, dB = True):
         """
+        Plot a 2D beam (cartesian).
+        
         This function plots the beam in a 2D cartesian plot.
         """
         fig,ax = plt.subplots(1,1,figsize=[20,10])
         
         if dB:
-            vmin = np.min(10*np.log10(self.beams_dir[i_pol,i_freq,:,:]))
-            vmax = np.max(10*np.log10(self.beams_dir[i_pol,i_freq,:,:]))
+            vmin = np.min(10*np.log10(self.directivity[i_pol,i_freq,:,:]))
+            vmax = np.max(10*np.log10(self.directivity[i_pol,i_freq,:,:]))
             cb_label = 'dB'
-            image2d = ax.imshow(10*np.log10(self.beams_dir[i_pol,i_freq]),vmin = vmin, vmax = vmax, extent = [0 , 180, 0 , 360])
+            image2d = ax.imshow(10*np.log10(self.directivity[i_pol,i_freq]),vmin = vmin, vmax = vmax, extent = [0 , 180, 0 , 360])
         else:
-            vmin = np.min(self.beams_dir[i_pol,i_freq,:,:])
-            vmax = np.max(self.beams_dir[i_pol,i_freq,:,:])
+            vmin = np.min(self.directivity[i_pol,i_freq,:,:])
+            vmax = np.max(self.directivity[i_pol,i_freq,:,:])
             cb_label = 'Intensity (arbitrary units)'
-            image2d = ax.imshow(self.beams_dir[i_pol,i_freq],vmin = vmin, vmax = vmax, extent = [0 , 180, 0 , 360])
+            image2d = ax.imshow(self.directivity[i_pol,i_freq],vmin = vmin, vmax = vmax, extent = [0 , 180, 0 , 360])
         
             
         ax.set_aspect('auto')
@@ -205,16 +255,27 @@ class CSTBeam():
         fig.colorbar(image2d, label=cb_label, ax=ax)
             
     def get_e_A(self,r):
+        """
+        Get the aperture efficiency.
+        
+        This function gets the aperture efficiency of the beam.
+        """
         A_p = np.pi * r ** 2
         return self.A_e / A_p
     
-    def fractional_power(self, theta_min = 0, theta_max = 180, phi_min = 0, phi_max = 90):
+    def fractional_power(self, theta_min = 0, theta_max = 180, phi_min = 0, phi_max = 360):
+        """
+        Get the fractional power.
+        
+        This function gets the power within a solid angle of the beam, normalized
+        by the total output power.
+        """
         scaling = np.abs( np.sin( self.theta * np.pi / 180))
-        total_power = np.sum(scaling * self.beams_dir, axis = (2,3))
+        total_power = np.sum(scaling * self.directivity, axis = (2,3))
         i_phi_min = int(phi_min//self.phi_step)
         i_phi_max = int(phi_max//self.phi_step)
         i_theta_min = int(theta_min//self.theta_step)
         i_theta_max = int(theta_max//self.theta_step)
-        power_within_angles = np.sum((scaling * self.beams_dir)[:,:,i_phi_min:i_phi_max,i_theta_min:i_theta_max], axis = (2,3))
+        power_within_angles = np.sum((scaling * self.directivity)[:,:,i_phi_min:i_phi_max,i_theta_min:i_theta_max], axis = (2,3))
         return power_within_angles / total_power
         
